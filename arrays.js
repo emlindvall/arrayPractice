7b4707f22dbd8609ef278b248757810a7569ee96
Em Lindvall <elindvall22@turing.edu>
2023-01-11 15:17:14 -0700

    Add var goodFoods, amountEaten, wasItGood

--- arrays.js
+++ arrays.js
@@ -1,4 +1,22 @@
-var goodFoods = ["pickles", "olives", "pineapple pizza", "tom kha", "dolmades"];
-goodFoods.pop ();
+// Storing Arrays, Accessing Elements: Part 1
+// 1. There are 5 elements in the array 
+// 2. Integer 99 has an index position of 100
+// 3. The value of the element in index position 3 is 90
+// 4. The value of the element in index position 0 is 87
+// 5. The index position of the last element in the array is 4
 
+
+
+
+var goodFoods = ["pickles", "papadum", "pineapple pizza", "tom kha", "dolmades"];
+var amountEaten = [2, 16, 4.5, 69, 420]
+var wasItGood = [true, false, true, true, false]
+
+goodFoods.pop();
 console.log(goodFoods);
+
+amountEaten.pop();
+console.log(amountEaten);
+
+wasItGood.pop();
+console.log(wasItGood);
