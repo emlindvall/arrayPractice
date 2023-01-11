4db2877fd71050ebfc71413590ba2d6eba5f21bb
Em Lindvall <elindvall22@turing.edu>
2023-01-11 15:32:53 -0700

    Add note about 3 array methods

--- arrays.js
+++ arrays.js
@@ -12,11 +12,14 @@ var goodFoods = ["pickles", "papadum", "pineapple pizza", "tom kha", "dolmades"]
 var amountEaten = [2, 16, 4.5, 69, 420]
 var wasItGood = [true, false, true, true, false]
 
+//Pop method will remove the last element of the array, "dolmades"
 goodFoods.pop();
 console.log(goodFoods);
 
-amountEaten.pop();
+//Push method will replace the last element of the array 420 with element 3
+amountEaten.push(3);
 console.log(amountEaten);
 
-wasItGood.pop();
+//Unshift method will replace the last element of the array false with element true
+wasItGood.unshift(true);
 console.log(wasItGood);
